a93c1406dc08040848bb32b759954c2a1b466c08
Shariq Shakeel <shariqshakeel107@gmail.com>
2025-01-12 16:45:07 +0500

    Javascript varaibles

---
+++ concepts/variables.js
@@ -0,0 +1,26 @@
+
+// variables
+
+const name = "Shariq";  //Constant
+let age = 19; //let
+var education = "BSCS"; // var 
+maritialStatus = "Single"; // not used any keyword
+
+// Changing all values
+
+
+age = 20;
+education = "Intermediate";
+maritialStatus = "Married";
+
+// printing all separately
+
+
+console.log(age);
+console.log(education);
+console.log(maritialStatus);
+
+// printing at once
+
+console.table([name,age,education,maritialStatus])
+
